b8147cfff9592a0ce0f5e640f52d553d0f096ae6
Miles Ranisavljevic <itstop3@gmail.com>
2015-12-07 12:04:46 -0800

    Initial commit

---
+++ StackExchangeClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackExchangeClient
+//
+//  Created by Miles Ranisavljevic on 12/7/15.
+//  Copyright © 2015 creeperspeak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
