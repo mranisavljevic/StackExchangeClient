211e04a2afddd585eaf2feb0482d4c31fde37b84
Miles Ranisavljevic <itstop3@gmail.com>
2015-12-07 12:13:05 -0800

    Add new view controllers and storyboard VCs.

---
+++ StackExchangeClient/BurgerMenuViewController.m
@@ -0,0 +1,37 @@
+//
+//  BurgerMenuViewController.m
+//  StackExchangeClient
+//
+//  Created by Miles Ranisavljevic on 12/7/15.
+//  Copyright © 2015 creeperspeak. All rights reserved.
+//
+
+#import "BurgerMenuViewController.h"
+
+@interface BurgerMenuViewController ()
+
+@end
+
+@implementation BurgerMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ StackExchangeClient/MenuTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuTableViewController.h
+//  StackExchangeClient
+//
+//  Created by Miles Ranisavljevic on 12/7/15.
+//  Copyright © 2015 creeperspeak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuTableViewController : UITableViewController
+
+@end
