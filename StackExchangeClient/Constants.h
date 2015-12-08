3fcd6f9907c1a622ad4abef3a11ef29f0714d050
Miles Ranisavljevic <itstop3@gmail.com>
2015-12-08 13:07:52 -0800

    Add constants file with typedefs for completion handlers, and JSON Request service files.

---
+++ StackExchangeClient/Constants.h
@@ -0,0 +1,17 @@
+//
+//  Constants.h
+//  StackExchangeClient
+//
+//  Created by Miles Ranisavljevic on 12/8/15.
+//  Copyright © 2015 creeperspeak. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+typedef void(^kNSDataCompletionHandler)(NSData *data, NSError *error);
+typedef void(^kNSDictionaryCompletionHandler)(NSDictionary *dictionary, NSError *error);
+typedef void(^kNSArrayCompletionHandler)(NSArray *array, NSError *error);
+typedef void(^kUIImageCompletionHandler)(UIImage *image, NSError *error);
+
+#endif /* Constants_h */
---
+++ StackExchangeClient/JSONRequestService.m
@@ -0,0 +1,40 @@
+//
+//  JSONRequestService.m
+//  StackExchangeClient
+//
+//  Created by Miles Ranisavljevic on 12/8/15.
+//  Copyright © 2015 creeperspeak. All rights reserved.
+//
+
+#import "JSONRequestService.h"
+#import <AFNetworking/AFNetworking.h>
+
+@implementation JSONRequestService
+
++ (void)GETRequestWithURLString:(NSString *)urlString parameters:(NSDictionary *)parameters completion:(kNSDataCompletionHandler)completion {
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    
+    NSOperationQueue *managerQueue = [manager operationQueue];
+    [manager.reachabilityManager setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        switch (status) {
+            case AFNetworkReachabilityStatusReachableViaWiFi:
+            case AFNetworkReachabilityStatusReachableViaWWAN:
+                [managerQueue setSuspended:NO];
+                break;
+            default:
+                [managerQueue setSuspended:YES];
+                break;
+        }
+    }];
+    
+    [manager GET:urlString parameters:parameters success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
+        NSData *data = (NSData *)responseObject;
+        completion(data, nil);
+    } failure:^(AFHTTPRequestOperation * _Nullable operation, NSError * _Nonnull error) {
+        completion(nil, error);
+    }];
+}
+
+@end
