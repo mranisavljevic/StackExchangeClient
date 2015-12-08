c3728ad8aa02cc37f8482c53940d5136189095db
Miles Ranisavljevic <itstop3@gmail.com>
2015-12-08 13:19:15 -0800

    Add StackOverflowSearchAPIService.

---
+++ StackExchangeClient/StackOverflowSearchAPIService.m
@@ -0,0 +1,37 @@
+//
+//  StackOverflowSearchAPIService.m
+//  StackExchangeClient
+//
+//  Created by Miles Ranisavljevic on 12/8/15.
+//  Copyright © 2015 creeperspeak. All rights reserved.
+//
+
+#import "StackOverflowSearchAPIService.h"
+#import "JSONRequestService.h"
+
+@implementation StackOverflowSearchAPIService
+
++ (void)searchQuestionsWithTerm:(NSString *)searchTerm page:(int)page completion:(kNSDictionaryCompletionHandler)completion {
+    NSString *searchURL = @"https://api.stackexchange.com/2.2/search";
+    
+    NSString *pageNumber = (page < 1 ? pageNumber = @"1" : [NSString stringWithFormat:@"%i", page]);
+    
+    NSMutableDictionary *parameters = [[NSMutableDictionary alloc] init];
+    [parameters setObject:pageNumber forKey:@"page"];
+    [parameters setObject:searchTerm forKey:@"intitle"];
+    [parameters setObject:@"stackoverflow" forKey:@"site"];
+    [parameters setObject:@"activity" forKey:@"sort"];
+    [parameters setObject:@"desc" forKey:@"order"];
+    
+    [JSONRequestService GETRequestWithURLString:searchURL parameters:parameters completion:^(NSData *data, NSError *error) {
+        if (error) {
+            completion(nil, error);
+            return;
+        }
+        NSDictionary *results = (NSDictionary *)data;
+        completion(results, nil);
+        return;
+    }];
+}
+
+@end
